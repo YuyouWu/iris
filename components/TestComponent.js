be598725e041af90df7d402a505fa5262a8397cb
YuyouWu <yuyouwu5@gmail.com>
2020-01-05 19:10:55 -0800

    Added TestComponent

---
+++ components/TestComponent.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+class TestComponent extends Component{
+    render(){
+        return (
+            <Text>
+                pp
+            </Text>
+        );
+    }
+};
+
+export default TestComponent;
