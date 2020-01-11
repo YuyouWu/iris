fc9918758c66079b9dc60fe73c56eddc6aa3daf2
YuyouWu <yuyouwu5@gmail.com>
2020-01-10 23:43:52 -0800

    Added icons to comments

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,23 +1,33 @@
-import React, {Component} from 'react';
-import { Text, View, ScrollView, Image, RefreshControl} from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, ScrollView, Image, RefreshControl } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/AntDesign';
 import axios from 'axios';
 
-class CommentList extends Component{
-    constructor(props){
+class CommentList extends Component {
+    constructor(props) {
         super(props);
     }
-    
-    render(){
+
+    renderTitle = () => {
+        return (
+            <View style={{ flexDirection: 'row' }}>
+                <Text style={{ color: 'grey', fontSize: 15, marginLeft: -15 }}>{this.props.comment.data.author} </Text>
+                <Text style={{ color: 'grey', fontSize: 15 }}>
+                    <Icon name='arrowup' color='grey'/>{this.props.comment.data.score}
+                </Text>
+            </View>
+        )
+    }
+
+    render() {
         return (
             <ListItem
-                title={this.props.comment.data.author}
+                title={this.renderTitle()}
                 subtitle={this.props.comment.data.body}
                 bottomDivider
                 containerStyle={{ backgroundColor: 'black' }}
-                titleStyle={{ color: 'grey', fontSize: 15, marginLeft:-15, marginRight:-15}}
-                subtitleStyle={{ color: 'white', fontSize: 15, marginLeft:-15, marginRight:-15 }}
+                subtitleStyle={{ color: 'white', fontSize: 15, marginLeft: -15, marginRight: -15 }}
             />
         );
     }
--- src/components/PostTileList.js
+++ src/components/PostTileList.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
-import { Text, View, ScrollView, Image, RefreshControl} from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, ScrollView, RefreshControl } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/AntDesign';
 import axios from 'axios';
 
 import PostThumbNail from './PostThumbNail';
 
-class PostTileList extends Component{
-    constructor(props){
+class PostTileList extends Component {
+    constructor(props) {
         super(props);
         this.state = {
             posts: null,
@@ -38,24 +38,24 @@ class PostTileList extends Component{
 
 
     PostSubtitle = (subreddit, score) => {
-        return(
-            <View style={{flexDirection: 'row'}}>
-                <Text style={{color: 'grey'}}>
+        return (
+            <View style={{ flexDirection: 'row' }}>
+                <Text style={{ color: 'grey' }}>
                     {subreddit + " "}
                 </Text>
-                <Text style={{color: 'grey'}}>
-                    <Icon name='arrowup' color='grey'/> {score}
+                <Text style={{ color: 'grey' }}>
+                    <Icon name='arrowup' color='grey' /> {score}
                 </Text>
             </View>
         );
     }
-    
-    render(){
+
+    render() {
         return (
             <ScrollView
                 refreshControl={
-                <RefreshControl refreshing={this.state.refreshing} onRefresh={this.onRefresh} />
-              }
+                    <RefreshControl refreshing={this.state.refreshing} onRefresh={this.onRefresh} />
+                }
             >
                 {this.state.posts &&
                     this.state.posts.map((post, i) => (
@@ -64,10 +64,10 @@ class PostTileList extends Component{
                             title={post.data.title}
                             subtitle={this.PostSubtitle(post.data.subreddit, post.data.score)}
                             bottomDivider
-                            titleStyle={{ color: 'white'}}
+                            titleStyle={{ color: 'white' }}
                             containerStyle={{ backgroundColor: 'black' }}
-                            leftElement={ <PostThumbNail thumbnailURL={post.data.thumbnail} linkURL={post.data.url} postHint={post.data['post_hint']} navigation={this.props.navigation}/> }
-                            onPress={ () => {
+                            leftElement={<PostThumbNail thumbnailURL={post.data.thumbnail} linkURL={post.data.url} postHint={post.data['post_hint']} navigation={this.props.navigation} />}
+                            onPress={() => {
                                 this.props.navigation.navigate('Post', {
                                     post: post
                                 });
