eed4c19a3cda3d915aa49767df52b48b30febee6
YuyouWu <yuyouwu5@gmail.com>
2020-01-23 23:07:08 -0800

    Added search container component

---
+++ src/components/SearchScreenComponents/SearchContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class SearchContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>This is the search Screen</Text>
+            </View>
+        );
+    }
+};
+
+export default SearchContainer;
