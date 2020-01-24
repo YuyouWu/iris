bb590cadc816e949299dd8fac6a90e943b72c16b
YuyouWu <yuyouwu5@gmail.com>
2020-01-23 22:49:32 -0800

    Bottom tab style change

--- src/components/BottomTabNavigator.js
+++ src/components/BottomTabNavigator.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs'
 import StackNavigator from './StackNavigator';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const BottomTabs = createBottomTabNavigator(
     {
         StackNavigator: {
             screen: StackNavigator,
             navigationOptions: {
-                title: 'Home'
+                title: 'Posts',
+                tabBarIcon: <Icon name="ios-albums" style={{color:"white"}} size={24}/>
             }
         }
     },
     {
-        initialRouteName: 'StackNavigator'
+        initialRouteName: 'StackNavigator',
+        tabBarOptions: {
+            activeTintColor: '#bfbfbf',
+            style:{
+                backgroundColor: '#1a1a1a'
+            }
+        }
     }
 );
 
