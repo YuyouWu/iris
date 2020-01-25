6ad19e2071057b7f1a88accf59b1dc608ace3605
Yuyou Wu <yuyouwu5@gmail.com>
2020-01-24 23:08:57 -0800

    Added input field and a list of popular subreddit

--- src/components/SearchScreenComponents/SearchContainer.js
+++ src/components/SearchScreenComponents/SearchContainer.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, ScrollView } from 'react-native';
+import { Input, ListItem } from 'react-native-elements';
+import axios from 'axios';
 
 class SearchContainer extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            subreddits: []
+        }
+
+        //Grabbing a list of popular subreddit by default
+        axios.get("https://www.reddit.com/subreddits/popular.json").then((res) => {
+            this.setState({
+                subreddits: res.data.data.children
+            });
+        });
     }
 
     render() {
         return (
-            <View>
-                <Text>This is the search Screen</Text>
-            </View>
+            <ScrollView>
+                <Input
+                    placeholder='Search for subreddits'
+                />
+                {this.state.subreddits &&
+                    this.state.subreddits.map((subreddit, i) => (
+                        <ListItem
+                            key={i}
+                            title={subreddit.data.display_name}
+                        />
+                    ))
+                }
+            </ScrollView>
         );
     }
 };
