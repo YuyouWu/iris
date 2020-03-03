735992e0e090d5d4ff49152ed9e71f73639ec95c
Yuyou Wu <yuyouwu5@gmail.com>
2020-03-02 22:27:07 -0800

    Installed AsyncStorage and initialize theme setting variable

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React from 'react';
 import 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import BottomTabNavigator from './src/components/BottomTabNavigator';
 
@@ -15,10 +16,32 @@ import BottomTabNavigator from './src/components/BottomTabNavigator';
 //TODO: Store theme obj into AsyncStorage if it's not done yet 
 //Retrive theme obj if it's there and set theme in styles files
 
-const App: () => React$Node = () => {
-  return (
-      <BottomTabNavigator/>
-  );
-};
+class App extends React.Component {
+  saveSetting = async () => {
+    try {
+      await AsyncStorage.setItem('@theme', 'dark')
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  initSetting = async () => {
+    try {
+      const theme = await AsyncStorage.getItem('@theme')
+      if(theme === null) {
+        this.saveSetting();
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    this.initSetting();
+    return (
+      <BottomTabNavigator />
+    )
+  }
+}
 
 export default App;
