ff32a2de69696de750d903fc70d53d97ff45584e
Yuyou Wu <yuyouwu5@gmail.com>
2020-03-07 20:12:49 -0800

    Adding light theme to post tile list

---
+++ src/components/PostScreenComponents/PostTileStackNavigator.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import 'react-native-gesture-handler';
+import { createStackNavigator, CardStyleInterpolators, TransitionSpecs } from '@react-navigation/stack';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import PostTileList from './PostTileList';
+import Post from './Post';
+import PostImage from './PostImage';
+import PostVideo from './PostVideo';
+import PostLinkView from './PostLinkView';
+
+import postTileStackStyle from '../../styles/postTileStackStyle';
+
+const PostTileStack = createStackNavigator();
+class RenderPostTileStack extends Component {
+    constructor() {
+        super();
+        this.state = {
+            theme: "dark",
+        }
+        this.loadTheme();
+    }
+
+    loadTheme = async () => {
+        try {
+            const theme = await AsyncStorage.getItem('@theme');
+            this.setState({
+                theme
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    render() {
+        return (
+            <PostTileStack.Navigator
+                initialRouteName="PostTileList"
+                screenOptions={{
+                    headerStyle: this.state.theme === "light" ? postTileStackStyle.lightHeaderStyle : postTileStackStyle.darkHeaderStyle,
+                    headerTintColor: this.state.theme === "light" ? postTileStackStyle.lightHeaderTintColor.color : postTileStackStyle.darkHeaderTintColor.color,
+                    cardStyle: this.state.theme === "light" ? postTileStackStyle.lightCardStyle : postTileStackStyle.darkCardStyle,
+                    headerTitleStyle: this.state.theme === "light" ? postTileStackStyle.lightHeaderTitleStyle : postTileStackStyle.darkHeaderTitleStyle,
+                    gestureEnabled: true,
+                    gestureDirection: "horizontal",
+                    cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+                    transitionSpec: {
+                        open: TransitionSpecs.TransitionIOSSpec,
+                        close: TransitionSpecs.TransitionIOSSpec,
+                    }
+                }}
+            >
+                <PostTileStack.Screen
+                    name="PostTileList"
+                    component={PostTileList}
+                    options={({ route }) => {
+                        let currentSub = 'all'
+                        if (route.params?.currentSub) {
+                            currentSub = route.params.currentSub;
+                        }
+                        return ({
+                            title: `r/${currentSub}`
+                        })
+                    }}
+                />
+                <PostTileStack.Screen
+                    name="Post"
+                    component={Post}
+                />
+                <PostTileStack.Screen
+                    name="PostImage"
+                    component={PostImage}
+                    options={{
+                        headerShown: false
+                    }}
+                />
+                <PostTileStack.Screen
+                    name="PostVideo"
+                    component={PostVideo}
+                    options={{
+                        headerShown: false
+                    }}
+                />
+                <PostTileStack.Screen
+                    name="PostLinkView"
+                    component={PostLinkView}
+                    options={{
+                        title: "Link"
+                    }}
+                />
+            </PostTileStack.Navigator>
+        )
+    }
+}
+
+export default RenderPostTileStack;
---
+++ src/styles/modalStyle.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export default listStyles = StyleSheet.create({
+    darkModalContainer: {
+        backgroundColor: "#1a1a1a"
+    },
+    lightModalContainer: {
+        backgroundColor: "white"
+    }
+})
